3eb664597f671ff90f8c9d3bc57a790947d4f60d
Kwts0s <zeus.primers@gmail.com>
2023-06-21 14:54:57 +0300

    Layout done

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 
 interface SidebarProps{
   children: React.ReactNode;
@@ -33,7 +34,7 @@ const routes = useMemo(() => [
 ],[pathname]);
 
   return (
-    <div>
+    
       <div className="flex h-full">
         <div className="
         hidden
@@ -64,12 +65,15 @@ const routes = useMemo(() => [
             </div>
           </Box>
           <Box className="overflow-y-auto h-full">
-            Song Library
+            <Library/>
           </Box>
 
         </div>
+        <main className="h-full flex-1 overflow-y-auto py-2">
+          {children}
+        </main>
       </div>
-    </div>
+    
   );
 }
 
